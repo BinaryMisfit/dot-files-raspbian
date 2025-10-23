a70b2ad2a9ac2a98def1d56b8e6aa4f25c87fa67
Willie Roberts <diagoza@me.com>
2025-10-23 13:02:30 +0200

    Fixed script

--- update-online.sh
+++ update-online.sh
@@ -10,8 +10,8 @@ fi
 echo "$(hostname) is running $(lsb_release -d -s) with kernel $(uname -r)."
 echo -ne "Config files, checking\r"
 if dig +short github.com &>/dev/null; then
-  if [[ -f $HOME/.dotfiles/update_check.sh ]]; then
-    source $HOME/.dotfiles/update_check.sh
+  if [[ -f $HOME/.dotfiles/update-check.sh ]]; then
+    source $HOME/.dotfiles/update-check.sh
   else
     echo -ne "\r\n"
   fi
